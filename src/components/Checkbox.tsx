f35c41c4bec801b83011f90e22f34809e9f764e0
Enrique <santosenrique2121@gmail.com>
2022-11-06 13:47:39 -0300

    feat: create component Checkbox

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,10 @@
+function Checkbox() {
+  return (
+    <input
+      type="checkbox"
+      className="w-4 h-4 accent-green-600 text-white checked:after:bg-white"
+    />
+  );
+}
+
+export default Checkbox;
